c9ab0052a7bc7e5ce857736f2a511c18d5fbecdc
Hemerson Carlin <mersocarlin@gmail.com>
2017-07-19 22:52:36 +0200

    (#3) Integrate prettier (#4)

--- spec/api/ping.spec.js
+++ spec/api/ping.spec.js
@@ -1,42 +1,34 @@
-import { expect } from 'chai';
-import request from 'supertest';
+import { expect } from 'chai'
+import request from 'supertest'
 
-import { application } from '../../src/';
-import { default as env } from '../../config/env';
-
+import { application } from '../../src/'
+import { default as env } from '../../config/env'
 
 describe('ping api', () => {
-  let app;
+  let app
 
-  before(async function () {
-    app = await application({ env });
-  });
+  before(async function() {
+    app = await application({ env })
+  })
 
   describe('/ping', () => {
     it('should return array for /api/ping', done => {
-      request(app)
-        .get('/api/ping')
-        .expect(200)
-        .end((err, res) => {
-          if (err) {
-            done(err);
-            return;
-          }
+      request(app).get('/api/ping').expect(200).end((err, res) => {
+        if (err) {
+          done(err)
+          return
+        }
 
-          expect(res.body).to.be.instanceof(Array);
-          res.body
-            .forEach((obj, index) => {
-              expect(obj).to.have.property('status', index);
-            });
-          done();
-        });
-    });
+        expect(res.body).to.be.instanceof(Array)
+        res.body.forEach((obj, index) => {
+          expect(obj).to.have.property('status', index)
+        })
+        done()
+      })
+    })
 
     it('show throw not found', done => {
-      request(app)
-        .get('/api/notfound')
-        .expect(404)
-        .end(done);
-    });
-  });
-});
+      request(app).get('/api/notfound').expect(404).end(done)
+    })
+  })
+})
--- src/api/ping/index.js
+++ src/api/ping/index.js
@@ -1,14 +1,7 @@
+function list(req, res) {
+  res.status(200).send([{ status: 0 }, { status: 1 }, { status: 2 }])
+}
 
 export default {
   list: list.bind(this),
-};
-
-function list (req, res) {
-  res
-    .status(200)
-    .send([
-      { status: 0 },
-      { status: 1 },
-      { status: 2 },
-    ]);
 }
